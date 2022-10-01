0f46c9d5f6636ebf69a1abbd4cdf8b1e699e5c63
diego <contact@fartweb.com>
2022-10-01 10:10:05 -0300

    d

--- frontend/src/paginas/nosotros.jsx
+++ frontend/src/paginas/nosotros.jsx
@@ -1,9 +1,12 @@
+import UncontrolledExample from "../componentes/footer";
+import SimpleBottomNavigation from "../componentes/footer";
 import NavBar from "../componentes/navBar";
 
 const Nosotros = () => {
   return (
     <>
       <NavBar />
+      <UncontrolledExample />
     </>
   );
 };
