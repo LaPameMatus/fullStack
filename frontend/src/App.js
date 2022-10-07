10c6cd93cfc70fe2bfb54e885bc9f7d3f25f57aa
diego <contact@fartweb.com>
2022-10-07 18:33:48 -0300

    diego cambio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,36 @@
-import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Navbar from './component/navegacion/Navbar';
-import Inicio from './component/paginas/Inicio';
-import Productos from './component/paginas/Productos';
-import Contactos from './component/paginas/Contactos';
-import Footer from './componentes/Footeer';
-
-
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./component/navegacion/Navbar";
+import Inicio from "./component/paginas/Inicio";
+import Productos from "./component/paginas/Productos";
+import Contactos from "./component/paginas/Contactos";
+import Footer from "./component/navegacion/Footeer";
 
 function App() {
   return (
-    <div className='fondo'>
-      <Router>    
-      <Navbar/>
-      <Switch>
-        <Route path='/frontend/src/component/paginas/Inicio.js' exact component ={Inicio}/>
-        <Route path='/frontend/src/component/paginas/Productos.js' exact component ={Productos}/>
-        <Route path='/frontend/src/component/paginas/Contactos.js' exact component ={Contactos}/>
-      </Switch>
-      <Footer />
-    </Router>
-    
+    <div className="fondo">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route
+            path="/frontend/src/component/paginas/Inicio.js"
+            exact
+            component={Inicio}
+          />
+          <Route
+            path="/frontend/src/component/paginas/Productos.js"
+            exact
+            component={Productos}
+          />
+          <Route
+            path="/frontend/src/component/paginas/Contactos.js"
+            exact
+            component={Contactos}
+          />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
-    
-    
-
-    
-
   );
 }
 
